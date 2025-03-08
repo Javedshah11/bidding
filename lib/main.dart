136b2d0fbd999fae285c88084fa8348d4bc4ecfb
Javedshah11 <167568357+Javedshah11@users.noreply.github.com>
2025-03-09 04:15:45 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-}
-@override
-Widget build(BuildContext context){
-  return MaterialApp(
-debugShowCheckedModeBanner: false,
-    title: 'Flutter Bidding application',
-    theme:ThemeData(
-      primarySwatch: Colors.green,
-    ),
-    home:const MaximumBid extends StatefulWidget(),
-  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Bidding application',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MaximumBid(),
+    );
+  }
 }
 
 class MaximumBid extends StatefulWidget {
-  const MaximumBid ({super.key});
+  const MaximumBid({super.key});
   @override
-  _MaximumBidState createState() => _MaximumBidstate();
-
+  _MaximumBidState createState() => _MaximumBidState();
 }
 
-class _MaximumBidState extends State <MaximumBid> {
-  int _bidAmount=100;
-  void _increaseBid(){
+class _MaximumBidState extends State<MaximumBid> {
+  int _bidAmount = 100;
+  void _increaseBid() {
     setState(() {
-      _bidAmount +=50;
+      _bidAmount += 50;
       debugPrint("Bid increased to:\$_bidamount");
     });
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: const Text("Bidding page"),
-      centerTitle: true,
+        centerTitle: true,
       ),
-body: Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text("current maxBid is:\\$_bidAmount",
-      style: const TextStyle(fontSize: 45,fontWeight: fontWeight.bold),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "current maxBid is:\\$_bidAmount",
+              style: const TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _increaseBid,
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text("increased bid by \$50"),
+            )
+          ],
+        ),
       ),
-      const SizedBox(height: 20),
-    ],
-  ),
-),
     );
   }
 }
